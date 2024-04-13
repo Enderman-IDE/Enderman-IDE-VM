fb343650e8c415c819493f5e281de0950eeb98fb
yumbee <156001240+CharcoalAfterlife@users.noreply.github.com>
2024-04-13 18:06:17 +0100

    turn internal

--- src/extensions/clock_joeu/index.js
+++ src/extensions/clock_joeu/index.js
@@ -1,157 +1,43 @@
+const formatMessage = require('format-message');
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+// const Cast = require('../../util/cast');
 
-(async function(Scratch) {
-    const variables = {};
-    const blocks = [];
-    const menus = [];
 
-
-    if (!Scratch.extensions.unsandboxed) {
-        alert("joe needs to go out of the sandbox, hes bored")
-        return
+/**
+ * Class 
+ * @constructor
+ */
+class JoeUtils {
+    constructor(runtime) {
+        /**
+         * The runtime instantiating this block package.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
     }
 
-    function doSound(ab, cd, runtime) {
-        const audioEngine = runtime.audioEngine;
-
-        const fetchAsArrayBufferWithTimeout = (url) =>
-            new Promise((resolve, reject) => {
-                const xhr = new XMLHttpRequest();
-                let timeout = setTimeout(() => {
-                    xhr.abort();
-                    reject(new Error("Timed out"));
-                }, 5000);
-                xhr.onload = () => {
-                    clearTimeout(timeout);
-                    if (xhr.status === 200) {
-                        resolve(xhr.response);
-                    } else {
-                        reject(new Error(`HTTP error ${xhr.status} while fetching ${url}`));
-                    }
-                };
-                xhr.onerror = () => {
-                    clearTimeout(timeout);
-                    reject(new Error(`Failed to request ${url}`));
-                };
-                xhr.responseType = "arraybuffer";
-                xhr.open("GET", url);
-                xhr.send();
-            });
-
-        const soundPlayerCache = new Map();
-
-        const decodeSoundPlayer = async (url) => {
-            const cached = soundPlayerCache.get(url);
-            if (cached) {
-                if (cached.sound) {
-                    return cached.sound;
+    /**
+     * @returns {object} metadata for this extension and its blocks.
+     */
+    getInfo() {
+        return {
+            id: 'joeu',
+            name: 'Joe Utils',
+            color1: "#ffa200",
+            color2: "#b87400",
+            blocks: [
+                {
+                    opcode: 'joe',
+                    blockType: BlockType.REPORTER,
+                    text: 'joe'
                 }
-                throw cached.error;
-            }
-
-            try {
-                const arrayBuffer = await fetchAsArrayBufferWithTimeout(url);
-                const soundPlayer = await audioEngine.decodeSoundPlayer({
-                    data: {
-                        buffer: arrayBuffer,
-                    },
-                });
-                soundPlayerCache.set(url, {
-                    sound: soundPlayer,
-                    error: null,
-                });
-                return soundPlayer;
-            } catch (e) {
-                soundPlayerCache.set(url, {
-                    sound: null,
-                    error: e,
-                });
-                throw e;
-            }
+            ]
         };
-
-        const playWithAudioEngine = async (url, target) => {
-            const soundBank = target.sprite.soundBank;
-
-            let soundPlayer;
-            try {
-                const originalSoundPlayer = await decodeSoundPlayer(url);
-                soundPlayer = originalSoundPlayer.take();
-            } catch (e) {
-                console.warn(
-                    "Could not fetch audio; falling back to primitive approach",
-                    e
-                );
-                return false;
-            }
-
-            soundBank.addSoundPlayer(soundPlayer);
-            await soundBank.playSound(target, soundPlayer.id);
-
-            delete soundBank.soundPlayers[soundPlayer.id];
-            soundBank.playerTargets.delete(soundPlayer.id);
-            soundBank.soundEffects.delete(soundPlayer.id);
-
-            return true;
-        };
-
-        const playWithAudioElement = (url, target) =>
-            new Promise((resolve, reject) => {
-                const mediaElement = new Audio(url);
-
-                mediaElement.volume = target.volume / 100;
-
-                mediaElement.onended = () => {
-                    resolve();
-                };
-                mediaElement
-                    .play()
-                    .then(() => {
-                        // Wait for onended
-                    })
-                    .catch((err) => {
-                        reject(err);
-                    });
-            });
-
-        const playSound = async (url, target) => {
-            try {
-                if (!(await Scratch.canFetch(url))) {
-                    throw new Error(`Permission to fetch ${url} denied`);
-                }
-
-                const success = await playWithAudioEngine(url, target);
-                if (!success) {
-                    return await playWithAudioElement(url, target);
-                }
-            } catch (e) {
-                console.warn(`All attempts to play ${url} failed`, e);
-            }
-        };
-
-        playSound(ab, cd)
     }
-    class Extension {
-        getInfo() {
-            return {
-                "id": "joeu",
-                "name": "Joe Utils",
-                "color1": "#ffa200",
-                "color2": "#b87400",
-                "tbShow": true,
-                "blocks": blocks
-            }
-        }
-    }
-    blocks.push({
-        opcode: `joe`,
-        blockType: Scratch.BlockType.REPORTER,
-        text: `Joe`,
-        arguments: {},
-        disableMonitor: true
-    });
-    Extension.prototype[`joe`] = async (args, util) => {
-        return 'joe'
-    };
 
-    Scratch.extensions.register(new Extension());
-})(Scratch);
+    joe () {
+        return 'joe';
+    }
+}
+module.exports = JoeUtils;
